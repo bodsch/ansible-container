1f2e97dabe5241557abe9adb2b1689a737d28e12
Bodo Schulz <bodo@boone-schulz.de>
2022-01-22 08:43:20 +0100

    implement first own module to create mounts and volumes directories

--- library/container_mounts.py
+++ library/container_mounts.py
@@ -18,7 +18,6 @@ from ansible.module_utils.basic import AnsibleModule
 class ContainerDirectories(object):
     """
     """
-
     def __init__(self, module):
         """
         """
@@ -49,14 +48,6 @@ class ContainerDirectories(object):
             'ro': True
         }
 
-        # _, _, self.uid, gid, _, _, _ = pwd.getpwnam(self.owner)
-        # name, passwd, num, mem = grp.getgrnam(self.group)
-        #
-        # if(not num):
-        #     self.gid = gid
-        # else:
-        #     self.gid = num
-
         self.uid = 1000
         self.gid = 1000
 
@@ -76,24 +67,48 @@ class ContainerDirectories(object):
             msg="initial"
         )
 
-        all_volumes = self.__volumes()
-        all_mounts  = self.__mounts()
+        all_mounts = []
+        all_volumes = []
+        migrated_volumes = []
 
-        migrated_volumes = self.__migrate_volumes(all_volumes)
+        if self.volumes:
+            all_volumes = self.__volumes()
+            migrated_volumes = self.__migrate_volumes(all_volumes)
+
+        if self.mounts:
+            all_mounts  = self.__mounts()
 
         full_list = migrated_volumes + all_mounts
+
+        if len(full_list) == 0:
+            return dict(
+                changed = False,
+                failed = False,
+                msg = "mothing to do"
+            )
 
         current_state = self.__analyse_directories(full_list)
         self.__create_directories(full_list, current_state)
 
         final_state = self.__analyse_directories(full_list)
 
-        self.module.log("{}".format(current_state))
-        self.module.log("{}".format(final_state))
+        # self.module.log("{}".format(current_state))
+        # self.module.log("{}".format(final_state))
 
-        equal_lists = self.__compare_two_lists(list1=current_state, list2=final_state)
+        equal_lists, diff = self.__compare_two_lists(list1=current_state, list2=final_state)
 
-        self.module.log("{}".format(equal_lists))
+        # self.module.log("{}".format(equal_lists))
+        # self.module.log("{}".format(diff))
+
+        if not equal_lists:
+            result['msg'] = "changed or created directories"
+            msg = ""
+            for i in diff:
+                msg += "- {0}\n".format(i)
+            result['created_directories'] = msg
+
+        if len(diff) == 0:
+            result['msg'] = "nothing to do"
 
         result['changed'] = not equal_lists
         result['failed'] = False
@@ -103,7 +118,7 @@ class ContainerDirectories(object):
     def __volumes(self):
         """
         """
-        self.module.log("- __volumes()")
+        # self.module.log("- __volumes()")
         all_volumes = []
 
         for d in self.data:
@@ -116,7 +131,7 @@ class ContainerDirectories(object):
     def __mounts(self):
         """
         """
-        self.module.log("- __mounts()")
+        # self.module.log("- __mounts()")
         all_mounts = []
 
         for d in self.data:
@@ -129,7 +144,7 @@ class ContainerDirectories(object):
     def __migrate_volumes(self, volumes):
         """
         """
-        self.module.log("- __migrate_volumes(volumes)")
+        # self.module.log("- __migrate_volumes(volumes)")
 
         result = []
         yaml = YAML()
@@ -197,10 +212,11 @@ class ContainerDirectories(object):
                 if count == 3 and values[2]:
                     read_mode = values[2]
 
-                if not (
-                    local_volume.endswith(self.volume_block_list_ends) or
-                    local_volume.startswith(self.volume_block_list_starts)
-                ):
+                valid = (local_volume.endswith(self.volume_block_list_ends) or local_volume.startswith(self.volume_block_list_starts))
+
+                if not valid:
+                    """
+                    """
                     res = dict(
                         source = local_volume,   # values[0],
                         target = remote_volume,  # values[1],
@@ -218,7 +234,7 @@ class ContainerDirectories(object):
     def __analyse_directories(self, directory_tree):
         """
         """
-        self.module.log("- __analyse_directories(directory_tree)")
+        # self.module.log("- __analyse_directories(directory_tree)")
         result = []
 
         # analyse first"
@@ -260,7 +276,7 @@ class ContainerDirectories(object):
     def __create_directories(self, directory_tree, current_state):
         """
         """
-        self.module.log("- __create_directories(directory_tree, current_state)")
+        # self.module.log("- __create_directories(directory_tree, current_state)")
 
         for entry in directory_tree:
             """
@@ -331,19 +347,24 @@ class ContainerDirectories(object):
 
         return None
 
-    def __compare_two_lists(self, list1: list, list2: list) -> bool:
+    def __compare_two_lists(self, list1: list, list2: list):
         """
         Compare two lists and logs the difference.
         :param list1: first list.
         :param list2: second list.
         :return:      if there is difference between both lists.
         """
-        diff = [i for i in list1 + list2 if i not in list1 or i not in list2]
+        # [i for i in list1 + list2 if i not in list1 or i not in list2]
+        diff = [x for x in list2 if x not in list1]
+
+        # self.module.log("  {0}".format(diff))
+        # self.module.log("  {0}".format(diff[:5]))
+
         result = len(diff) == 0
         if not result:
             self.module.log("There are {0} differences:".format(len(diff)))
             self.module.log("  {0}".format(diff[:5]))
-        return result
+        return result, diff
 
 
 # ===========================================
