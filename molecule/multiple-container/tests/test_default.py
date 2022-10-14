7dd9ccc17d68d144e82eaebf9ccd220166461b26
Bo.Sch <bodo@boone-schulz.de>
2022-10-14 08:41:24 +0200

    use ansible FQDN (#14)
    * use ansible FQDN
    * add new tests to demonstrate update properties and container
    * update github action

---
+++ molecule/multiple-container/tests/test_default.py
@@ -0,0 +1,98 @@
+
+from ansible.parsing.dataloader import DataLoader
+from ansible.template import Templar
+import pytest
+import os
+import testinfra.utils.ansible_runner
+
+import pprint
+pp = pprint.PrettyPrinter()
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def base_directory():
+    cwd = os.getcwd()
+
+    if('group_vars' in os.listdir(cwd)):
+        directory = "../.."
+        molecule_directory = "."
+    else:
+        directory = "."
+        molecule_directory = "molecule/{}".format(os.environ.get('MOLECULE_SCENARIO_NAME'))
+
+    return directory, molecule_directory
+
+
+@pytest.fixture()
+def get_vars(host):
+    """
+
+    """
+    base_dir, molecule_dir = base_directory()
+
+    file_defaults = "file={}/defaults/main.yml name=role_defaults".format(base_dir)
+    file_vars = "file={}/vars/main.yml name=role_vars".format(base_dir)
+    file_molecule = "file={}/group_vars/all/vars.yml name=test_vars".format(molecule_dir)
+
+    defaults_vars = host.ansible("include_vars", file_defaults).get("ansible_facts").get("role_defaults")
+    vars_vars = host.ansible("include_vars", file_vars).get("ansible_facts").get("role_vars")
+    molecule_vars = host.ansible("include_vars", file_molecule).get("ansible_facts").get("test_vars")
+
+    ansible_vars = defaults_vars
+    ansible_vars.update(vars_vars)
+    ansible_vars.update(molecule_vars)
+
+    templar = Templar(loader=DataLoader(), variables=ansible_vars)
+    result = templar.template(ansible_vars, fail_on_undefined=False)
+
+    return result
+
+
+def test_directory(host, get_vars):
+    dir = host.file(get_vars.get('container_env_directory'))
+    assert dir.exists
+    assert dir.is_directory
+
+
+@pytest.mark.parametrize("files", [
+    "nginx-proxy",
+    "whoami",
+    "hello-world"
+])
+def test_environments(host, get_vars, files):
+    dir = host.file(get_vars.get('container_env_directory'))
+
+    for file in [
+        "{0}/{1}/container.env".format(dir.linked_to, files),
+        "{0}/{1}/.container.env.checksum".format(dir.linked_to, files)
+    ]:
+        f = host.file(file)
+        assert f.is_file
+
+    # f = host.file("{0}/{1}/container.env".format(dir.linked_to, files))
+    # assert f.is_file
+    #
+    # f = host.file("{0}/{1}/container.env".format(dir.linked_to, files))
+    # assert f.is_file
+
+
+@pytest.mark.parametrize("files", [
+    "hello-world"
+])
+def test_properties(host, get_vars, files):
+    dir = host.file(get_vars.get('container_env_directory'))
+
+    for file in [
+        "{0}/{1}/{1}.properties".format(dir.linked_to, files),
+        "{0}/{1}/.{1}.properties.checksum".format(dir.linked_to, files)
+    ]:
+        f = host.file(file)
+        assert f.is_file
+
+    # f = host.file("{0}/{1}/{1}.properties".format(dir.linked_to, files))
+    # assert f.is_file
+    #
+    # f = host.file("{0}/{1}/.{1}.properties.checksum".format(dir.linked_to, files))
+    # assert f.is_file
