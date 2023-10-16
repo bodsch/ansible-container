d63ca64c875e94bd49716fceea647455b131bbc1
Bodo Schulz <bodo@boone-schulz.de>
2023-10-16 07:35:49 +0200

    add workflow to delete older runs

--- hooks/tox.sh
+++ hooks/tox.sh
@@ -9,18 +9,41 @@ then
   for collection in $(grep -v "#" collections.yml | grep "^  - name: " | awk -F ': ' '{print $2}')
   do
     collections_installed="$(ansible-galaxy collection list | grep ${collection} 2> /dev/null)"
-    #version=$(yq -r \
-    #  ".collections[] | select(.name == \"${collection}\") | .version" collections.yml)
 
     if [ -z "${collections_installed}" ]
     then
-      echo "Install the required collection '${collection}'"
-
-      ansible-galaxy collection install ${collection}
+      install_collection ${collection}
     else
       collection_version=$(echo "${collections_installed}" | awk -F ' ' '{print $2}')
+      version="$(grep -v "#" collections.yml | grep -A1 "^  - name: ${collection}" | grep "version: " 2> /dev/null | awk -F ': ' '{print $2}' | sed -e 's|=||' -e 's|>||' -e 's|"||g')"
 
       echo "The required collection '${collection}' is installed in version ${collection_version}."
+
+      if [ ! -z "${version}" ]
+      then
+
+        vercomp "${version}" "${collection_version}"
+
+        case $? in
+          0) op='=' ;;
+          1) op='>' ;;
+          2) op='<' ;;
+        esac
+
+        if [[ $op = "=" ]] || [[ $op = ">" ]]
+        then
+          # echo "FAIL: Expected '$3', Actual '$op', Arg1 '$1', Arg2 '$2'"
+          echo "re-install for version ${version}"
+
+          remove_collection ${collection}
+          install_collection ${collection}
+        else
+          :
+          # echo "Pass: '$1 $op $2'"
+        fi
+      else
+        :
+      fi
     fi
   done
   echo ""
